51e290052a4cf8684aa0bea9b84e779b7884a7e8
sweenutt <sweenutt@192.168.3.19>
2020-10-20 02:00:17 +0700

    Latihan 7 Bilangan Integer

---
+++ src/pbo2/if2/pkg10116414/latihan7_integer/PBO2IF210116414Latihan7_Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.if2.pkg10116414.latihan7_integer;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * DESKRIPSI    : Program Bilangan Integer
+ */
+public class PBO2IF210116414Latihan7_Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      byte b;
+      short s;
+      int i;
+      long l;
+      b = 120;
+      s = 32767;
+      i = 1_000_000_000;
+      l = 90000000000L;
+      System.out.println("byte \t:" +b);
+      System.out.println("short \t:" + s);
+      System.out.println("int \t:" +i);
+      System.out.println("long \t:" +l);
+    }
+    
+}
